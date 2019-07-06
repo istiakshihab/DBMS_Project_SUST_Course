d9b1db2f4f84359bc42293b03696468094010d0a
istiakshihab <shihabistiak@gmail.com>
2019-07-06 15:57:56 +0600

    Merge branch 'master' of https://github.com/Rough22/Project_Website
    # Conflicts:
    #	dbms.sql

---
+++ accounts/forms.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+from accounts.models import Student, User
+
+
+class StudentSignUpForm(UserCreationForm):
+    sess = forms.CharField(label='Session')
+    
+    class Meta(UserCreationForm.Meta):
+        model = User
+        help_texts = {
+            'username': 'Enter your Registration Number'
+        }
+
+    def save(self):
+        user = super().save(commit=False)
+        user.is_student = True
+        user.save()
+        student = Student.objects.create(user=user)
+        student.sessionStudent = self.cleaned_data['sess']
+        student.save()
+        return user
+
+
+class TeacherSignUpForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        help_texts = {
+            'username': 'Enter your Name code.'
+        }
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_teacher = True
+        if commit:
+            user.save()
+        return user
---
+++ accounts/views/teachers.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import login
+from django.shortcuts import redirect
+from django.views.generic import CreateView
+
+from ..forms import TeacherSignUpForm
+from ..models import User
+
+
+class TeacherSignUpView(CreateView):
+    model = User
+    form_class = TeacherSignUpForm
+    template_name = 'accounts/signup_form.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'teacher'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
