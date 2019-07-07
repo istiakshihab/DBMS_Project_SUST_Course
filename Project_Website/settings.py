5ac4b6eb7993dfe52451f011fa071a2f594b244d
istiakshihab <shihabistiak@gmail.com>
2019-07-07 17:13:25 +0600

    Safety Commit

--- Project_Website/settings.py
+++ Project_Website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'crispy_forms',
     'others.apps.ProductsConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -137,7 +139,9 @@ AUTH_USER_MODEL = 'accounts.User'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Project_Website/static/')
 ]
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+django_heroku.settings(locals())
