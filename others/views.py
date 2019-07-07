48fdbf17335156159103fb056082be7e1f5ccaa2
istiakshihab <shihabistiak@gmail.com>
2019-07-07 06:27:04 +0600

    Safety Commit

--- others/views.py
+++ others/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponseForbidden, HttpResponseRedirect, HttpRespons
 from django.shortcuts import render, redirect, get_object_or_404
 
 from others.models import *
-from accounts.models import *
 
 
 def home(request):
@@ -72,7 +71,7 @@ def course_detail(request, course_id):
     if request.user.is_authenticated:
         if request.user.is_teacher:
             course_obj = get_object_or_404(Course, course_id=course_id)
-            return HttpResponse('<h1>course_id</h1>')
+            return render(request, 'products/course.html')
         else:
             return HttpResponse('<h1>course_id</h1>')
     else:
