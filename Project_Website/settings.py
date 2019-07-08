82bf98fc6366bd127c19f98470f65beb3f02b36d
roytirtha <46457597+roytirtha@users.noreply.github.com>
2019-07-09 00:54:03 +0600

    ih

--- Project_Website/settings.py
+++ Project_Website/settings.py
@@ -80,22 +80,13 @@ WSGI_APPLICATION = 'Project_Website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'USER': 'root',
         'PASSWORD': '',
         'NAME': 'teamssfc',
         'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
-            'charset': 'utf8mb4',
-        },
-        # Tell Django to build the test database with the 'utf8mb4' character set
-        'TEAMSSFC': {
-            'CHARSET': 'utf8mb4',
-            'COLLATION': 'utf8mb4_unicode_ci',
-        }
+        'PORT': '5432',
+        
     }
 }
 
