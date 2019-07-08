7d4a2fe0cb071966175049107e98e7bd75e49940
roytirtha <46457597+roytirtha@users.noreply.github.com>
2019-07-09 00:45:26 +0600

    ih

--- Project_Website/settings.py
+++ Project_Website/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'Project_Website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql',
         'USER': 'root',
         'PASSWORD': '',
         'NAME': 'teamssfc',
