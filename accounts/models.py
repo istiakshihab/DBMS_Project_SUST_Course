46b01c229253af1b5cc0441956e0aa9828383df3
Rough022 <firozahmed651@yahoo.com>
2019-07-12 22:18:55 +0600

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -6,14 +6,23 @@ class User(AbstractUser):
     is_student = models.BooleanField(default=False)
     is_teacher = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.username
+
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     sessionStudent = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.user
+
 
 class Teacher(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return self.user
 
 
 
