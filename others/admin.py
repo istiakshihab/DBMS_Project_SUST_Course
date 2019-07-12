ccef12c46362c57fe6a8aefe773dbd655f366117
Rough022 <firozahmed651@yahoo.com>
2019-07-12 22:07:48 +0600

    Update admin.py

--- others/admin.py
+++ others/admin.py
@@ -1,1 +1,10 @@
 # Register your models here.
+from django.contrib import admin
+
+from .models import *
+
+admin.site.register(Course)
+admin.site.register(OfferedCourse)
+admin.site.register(Task)
+admin.site.register(TaskTeam)
+admin.site.register(Team)
